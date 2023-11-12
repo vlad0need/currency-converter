e1ee0dcc3fa418be25be985ad73b3a053d82032e
vlad0need <ov4lil@icloud.com>
2023-11-12 13:55:09 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,11 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root')  as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Provider store={store}>
+  <Provider store={store}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </Provider>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
 );
